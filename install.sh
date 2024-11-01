24dfc81723cf8a1e0433abf483125c7a60ab3a11
fumofumoenjoyer <45699815+fumofumoenjoyer@users.noreply.github.com>
2024-11-01 05:54:46 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+cd
+sudo pacman -Syu --needed base-devel git
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd
+yay -S sddm qtile kitty zsh zsh-autosuggestions zsh-syntax-highlighting oh-my-posh thunar thunar-archive-plugin thunar-volman tumbler gvfs ark p7zip fastfetch picom rofi-wayland polkit-gnome pipewire playerctl brightnessctl waypaper feh swaybg flameshot imagemagick alsa-utils python-iwlib python-psutil python-pywlroots qtile-extras python-dbus-next xdg-utils nerd-fonts flatpak lxappearance htop tty-clock unimatrix gtk-engine-murrine tokyonight-gtk-theme-git papirus-icon-theme-git librewolf-bin kvantum kvantum-qt5 kvantum-theme-catppuccin-git selectdefaultapplication-git mission-center xorg-xwayland dunst
+cd fumofumo
+mv -t ~/.config dunst fastfetch kitty picom qtile rofi Thunar xfce4
+mv -t ~ Pictues
+cd ~/fumofumo/zsh
+mv histfile .histfile
+mv zshrc .zshrc
+mv -t ~ .histfile prompt.json .zshrc
+chsh -s /bin/zsh
