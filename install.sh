23dfe7d1837c44769f46ce9027fb0931d96aff36
fumofumoenjoyer <45699815+fumofumoenjoyer@users.noreply.github.com>
2024-11-06 05:14:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ makepkg -si
 cd
 yay -S xorg sddm qtile kitty zsh zsh-autosuggestions zsh-syntax-highlighting xwaylandvideobridge oh-my-posh thunar thunar-archive-plugin thunar-volman tumbler gvfs ark p7zip fastfetch picom rofi-wayland polkit-gnome pipewire playerctl brightnessctl waypaper feh swaybg flameshot imagemagick alsa-utils python-iwlib python-psutil python-pywlroots qtile-extras python-dbus-next xdg-utils nerd-fonts flatpak lxappearance htop tty-clock unimatrix gtk-engine-murrine tokyonight-gtk-theme-git papirus-icon-theme-git librewolf-bin kvantum kvantum-qt5 kvantum-theme-catppuccin-git selectdefaultapplication-git mission-center xorg-xwayland dunst timeshift-systemd-timer timeshift
 cd fumofumo
-mv -t ~/.config dunst fastfetch kitty picom qtile rofi Thunar xfce4
+mv -t ~/.config dunst fastfetch kitty picom hypr qtile rofi Thunar xfce4
 mv -t ~ Pictures
 cd ~/fumofumo/zsh
 mv histfile .histfile
