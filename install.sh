819e5b82d4c8349bddf3c7412453ac17d832d9e0
fumofumoenjoyer <45699815+fumofumoenjoyer@users.noreply.github.com>
2024-11-15 15:36:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd
-yay -S xorg sddm qtile kitty zsh zsh-autosuggestions zsh-syntax-highlighting xwaylandvideobridge oh-my-posh thunar thunar-archive-plugin thunar-volman tumbler gvfs ark p7zip fastfetch picom rofi-wayland polkit-gnome pipewire playerctl brightnessctl waypaper feh swaybg flameshot imagemagick alsa-utils python-iwlib python-psutil python-pywlroots qtile-extras python-dbus-next xdg-utils nerd-fonts flatpak lxappearance htop tty-clock unimatrix gtk-engine-murrine tokyonight-gtk-theme-git papirus-icon-theme-git librewolf-bin kvantum kvantum-qt5 kvantum-theme-catppuccin-git selectdefaultapplication-git mission-center xorg-xwayland dunst timeshift-systemd-timer timeshift
+yay -S xorg sddm qtile kitty zsh zsh-autosuggestions zsh-syntax-highlighting xwaylandvideobridge oh-my-posh thunar thunar-archive-plugin thunar-volman tumbler gvfs ark p7zip fastfetch picom rofi-wayland polkit-gnome pipewire playerctl brightnessctl waypaper feh swaybg flameshot imagemagick alsa-utils python-iwlib python-psutil python-pywlroots qtile-extras python-dbus-next xdg-utils nerd-fonts flatpak lxappearance htop tty-clock unimatrix gtk-engine-murrine tokyonight-gtk-theme-git papirus-icon-theme-git librewolf-bin kvantum kvantum-qt5 kvantum-theme-catppuccin-git selectdefaultapplication-git mission-center xorg-xwayland dunst
 rm -rf ~/.config/dunst
 rm -rf ~/.config/fastfetch
 rm -rf ~/.config/kitty
@@ -23,5 +23,4 @@ mv zshrc .zshrc
 mv -t ~ .histfile prompt.json .zshrc
 chsh -s /bin/zsh
 sudo systemctl daemon-reload
-sudo systemctl enable timeshift-boot.timer
 sudo systemctl enable sddm
